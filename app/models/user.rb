85c92f3ccf6476ab99865359904e6f02fb36f51c
huntertparks <hunter.thomas.parks@gmail.com>
2018-08-15 20:04:59 -0700

    implement user with associations and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  # Encrypt user password
+  has_secure_password
+
+  # Model Associations
+  has_many :todos, foreign_key: :created_by
+  # Validations
+  validates_presence_of :name, :email, :password_digest
 end
